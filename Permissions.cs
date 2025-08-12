55683157bf17bdfd7082787e5ab62a310e289d9a
slissill <slissill@gmail.com>
2025-08-12 12:58:55 +0200

    Cleaning Warnings

--- Permissions.cs
+++ Permissions.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Maui.Controls.PlatformConfiguration;
+﻿using System.Runtime.Versioning;
 #if ANDROID
 using Android;
 using Android.Content.PM;
@@ -11,36 +6,47 @@ using Android.OS;
 using AndroidX.Core.App;
 using AndroidX.Core.Content;
 #endif
+
 namespace Ruquier;
 
 public static class Permissions
 {
+#if ANDROID
+  [SupportedOSPlatform("android30.0")]
+  private static bool IsExternalStorageManager()
+  {
+    return Android.OS.Environment.IsExternalStorageManager;
+  }
+#endif
 
   public static void RequestStoragePermission()
   {
 #if ANDROID
     var activity = Platform.CurrentActivity;
-    if (Build.VERSION.SdkInt >= BuildVersionCodes.R)
+    if (activity == null)
+      return; // Ou gérer l'absence d'activité
+
+    if (Build.VERSION.SdkInt >= BuildVersionCodes.R) // API 30+
     {
-      if (!Android.OS.Environment.IsExternalStorageManager)
+#pragma warning disable CA1416 // Validate platform compatibility
+      if (!IsExternalStorageManager())
       {
         var intent = new Android.Content.Intent(Android.Provider.Settings.ActionManageAllFilesAccessPermission);
         activity.StartActivity(intent);
       }
+#pragma warning restore CA1416
     }
-    else
+    else // API < 30
     {
       if (ContextCompat.CheckSelfPermission(activity, Manifest.Permission.ReadExternalStorage) != Permission.Granted)
       {
         ActivityCompat.RequestPermissions(activity, new string[]
         {
-                  Manifest.Permission.ReadExternalStorage,
-                  Manifest.Permission.WriteExternalStorage
+                    Manifest.Permission.ReadExternalStorage,
+                    Manifest.Permission.WriteExternalStorage
         }, 1);
       }
     }
 #endif
   }
-
-
 }
